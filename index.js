af23ba014b680b4bb211d19a0dbf55e8f5df2c26
marc espinosa <97604443+N3LOWI@users.noreply.github.com>
2023-05-11 13:31:47 +0200

    commit crud

--- index.js
+++ index.js
@@ -1,91 +1,39 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
-////////////////////////////////////////////////////////////////////////////////////////////////
+app.use(express.json());
 
-/**************************************************************************** */
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sachista:12345@cluster0.ljclkgv.mongodb.net/alumnistlab?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
 
+require('dotenv').config();
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+const mongoString = process.env.DATABASE_URL
 
-const User = mongoose.model('User', userSchema);
-/**************************************************************************** */
+//random
+mongoose.set('strictQuery', false);
+//random
+mongoose.connect(mongoString);
+const database = mongoose.connection
 
-app.post('/register', async (req, res) => {
-  try {
-    // Hash and salt the user's password
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+database.on('error', (error) => {
+    console.log(error)
+})
 
-    // Create a new user object
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword
-    });
+database.once('connected', () => {
+    console.log('DATABASE CONNECTED !!!');
+})
 
-    // Save the user object to the database
-    const newUser = await user.save();
+const routes = require('./routes/routes');
 
-    // Return a success response
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    // Return an error response
-    res.status(500).json({ message: 'An error occurred while registering the user' });
-  }
-});
+app.use('/api', routes)
 
 
 
 
+//routes
 
-
-
-
-
-
-
-
-
-app.post('/login', async (req, res) => {
-  try {
-
-    // Check if the password is correct
-    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
-
-    if (passwordMatch) {
-      // Create a JSON Web Token with the user's username as the payload
-      const token = jwt.sign({ username: user.username }, 'secret');
-
-      // Return the token in the response
-      res.status(200).json({ token: token });
-    } else {
-      // Return an error response
-      res.status(401).json({ message: 'idk' });
-    }
-  } catch (error) {
-    // Return an error response
-    res.status(500).json({ message: 'An error occurred while loging the user' });
-  }
-});
-
-
-
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
 
