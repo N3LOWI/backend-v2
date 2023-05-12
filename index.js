316fac80c9ada9b5f924e2fd308fae629ef5d911
marc espinosa <97604443+N3LOWI@users.noreply.github.com>
2023-05-12 15:22:26 +0200

    CORS fix 2

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
+
+//Allow CORS from any website
+app.use(cors({
+    origin: '*'
+}));
+
+
+
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -28,11 +36,17 @@ database.once('connected', () => {
     console.log('DATABASE CONNECTED !!!');
 })
 
+
+
 const routesAlum = require('./routes/routes-alum');
 const routesProfe = require('./routes/routes-profe');
 
 app.use('/api', routesAlum);
 app.use('/api', routesProfe);
 
-app.use(cors());
-
+/*
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});*/
