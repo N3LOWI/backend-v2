5c2b8d031f08edcd870e80527bda5ddd5de719f5
marc espinosa <97604443+N3LOWI@users.noreply.github.com>
2023-05-26 20:59:37 +0200

    buffer to string bug fix

--- model/model-noti.js
+++ model/model-noti.js
@@ -8,30 +8,7 @@ const notiSchema = new mongoose.Schema({
     },
     imagen: {
         required: false,
-        type: Buffer,
-        validate: {
-            validator: function (value) {
-                // Check if value is empty (no buffer)
-                if (!value || !Buffer.isBuffer(value)) {
-                    return true; // Accept empty field
-                }
-    
-                // Check if the image is in JPEG format
-                const jpegSignature = Buffer.from([0xff, 0xd8, 0xff]);
-                if (!value.slice(0, 3).equals(jpegSignature)) {
-                    return false;
-                }
-    
-                // Check if the image size is less than or equal to 10MB
-                const maxSize = 10 * 1024 * 1024; // 10MB in bytes
-                if (value.length > maxSize) {
-                    return false;
-                }
-    
-                return true;
-            },
-            message: 'The image must be in JPEG format and not exceed 10MB.'
-        }
+        type: String,
     },
     
     cuerpo: {
